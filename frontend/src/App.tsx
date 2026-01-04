21feedb066d37ab6f527a1a24fa55a1c3f08782d
lazyaashish <aashishraj.spds@gmail.com>
2026-01-04 23:08:46 +0530

    Added navbar,Improve landing page UI, animations, and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,15 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing/LandingPage";
-import "./App.css";
 import SignIn from "./components/SignIn";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: 12 }}>
-        <Link to="/" style={{ marginRight: 12 }}>
-          Home
-        </Link>
-        <Link to="/signin">Sign In</Link>
-      </nav>
-
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignIn />} /> //ADD SIGNUP HERE ONCE CREATED
       </Routes>
     </BrowserRouter>
   );
