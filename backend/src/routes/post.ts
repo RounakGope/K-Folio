97d35176cfc14efe95c64a7e9fc45b1cb29935f1
rounak <rounakgope50@gmail.com>
2026-01-06 13:05:36 +0530

    issues resolved

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { createPost } from "../controllers/postController";
-import { authenticate } from "../middlewares/authenticate";
-import { validate } from "../middlewares/validate";
+
 import { createPostSchema } from "../validators/postSchema";
 
 const router = Router();
 
-router.post("/", authenticate, validate(createPostSchema), createPost);
+router.post("/", createPost);
 
 export default router;
